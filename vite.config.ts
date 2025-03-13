3568db663ec96bf2f487850c73c308267364d6e1
shovit thapa <shovit.thapa@storaenso.com>
2025-03-13 10:46:41 +0200

    unused improt erro fixes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import createSvgSpritePlugin from 'vite-plugin-svg-sprite';
 
 export default defineConfig({
   plugins: [
